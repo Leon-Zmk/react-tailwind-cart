909c27bdc5485dfc9c16ea105440c9e0eb4c01ea
Leon <leonzmkdev@gmail.com>
2023-06-27 02:23:59 +0630

    detail

---
+++ src/components/Detail.jsx
@@ -0,0 +1,28 @@
+import React, { useRef } from 'react'
+import { useState , useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+
+function Detail() {
+   
+    const {id}=useParams();
+    const [item,setItem]=useState({})
+
+    useEffect(()=>{
+      getProduct();
+    },[])
+  
+    const getProduct=async()=>{
+      const data= await fetch("https://fakestoreapi.com/products/"+id)
+      const api= await data.json();
+      setItem(api);
+      
+    }
+    
+  return (
+    <div>
+       <p className='text-center text-teal-300 '>{item.title}</p>
+    </div>
+  )
+}
+
+export default Detail
