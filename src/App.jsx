fe69d01d15886ef2d050b09a2946a9ab62389d6a
Leon <leonzmkdev@gmail.com>
2023-06-27 02:23:52 +0630

    fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Routes,Route } from 'react-router-dom'
-import Home from './Home'
 import Navbar from './components/Navbar'
+import Products from  './components/Products'
+import Detail from './components/Detail'
 
 function App() {
+
+  const [items,setItem]=useState([])
+
+  useEffect(()=>{
+    getProducts();
+  },[])
+
+
+  const getProducts=async()=>{
+    const data= await fetch("https://fakestoreapi.com/products")
+    const api= await data.json();
+    setItem(api);
+
+  }
+
+
   return (
     <div>
       <Navbar></Navbar>
       <Routes>
-        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/' element={<Products products={items} />}></Route>
+        <Route path="/detail/:id" element={<Detail />}></Route>
       </Routes>
     </div>
   )
